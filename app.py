b287d2f4719daa4f914f133e741134b3c5dccad5
baonguyendang199-ui <baonguyendang199@gmail.com>
2025-10-24 02:39:42 +0700

    ád

--- app.py
+++ app.py
@@ -553,7 +553,8 @@ def main():
             column_config={
                 "STT": st.column_config.NumberColumn("STT", width="small"),
                 "Nation": st.column_config.TextColumn("Quốc gia", width="small"),
-                "Count": st.column_config.NumberColumn("Số lượng", width="small")
+                "Count": st.column_config.NumberColumn("Số lượng", width="small"),
+            }
 
     elif current_tab == 'skills':
         st.header("🎮 Quản lý Skills")
