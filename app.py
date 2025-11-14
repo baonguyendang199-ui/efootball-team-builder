02744144ee30ab5f2ae7846a39df0666edac6135
baonguyendang199-ui <baonguyendang199@gmail.com>
2025-11-14 16:04:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import streamlit as st
 import json
 from google.oauth2.service_account import Credentials
 import gspread
+import cloudscraper
 
 # --- GOOGLE SHEETS CONNECTION ---
 @st.cache_resource
